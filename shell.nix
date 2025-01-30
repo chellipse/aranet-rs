88a03ea1a10851e642138b1a1718e28bc633a83f
Chell L. <pumpkinbiscuit@protonmail.com>
2025-01-30 02:11:14 +0000

    * closer to interacting with characteristics

--- shell.nix
+++ shell.nix
@@ -1,12 +1,12 @@
 let
   rust_overlay = import (builtins.fetchTarball {
-    url = "https://github.com/oxalica/rust-overlay/archive/573c674a3ad06e8a525263185ebef336a411d1d5.tar.gz";
-    sha256 = "1bp1k5qla5gwh6vc50m5pcwdfxn6g703yx1i8qrjs4l7kgh3y507";
+    url = "https://github.com/oxalica/rust-overlay/archive/6a3dc6ce4132bd57359214d986db376f2333c14d.tar.gz"; # master
+    sha256 = "1v4h0fiifb7n3cfaqpnkkd4ynbn8ygypcwa92n5k3klyyqcy4nqq";
   });
   
   pkgs = import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/d3c42f187194c26d9f0309a8ecc469d6c878ce33.tar.gz";
-    sha256 = "0bmnxsn9r4qfslg4mahsl9y9719ykifbazpxxn1fqf47zbbanxkh";
+    url = "https://github.com/NixOS/nixpkgs/archive/9d3ae807ebd2981d593cddd0080856873139aa40.tar.gz"; # nixos-unstable
+    sha256 = "0bjqgsprq9fgl5yh58dk59xmchi4dajq3sf5i447q02dbiasjsil";
   }) { overlays = [ rust_overlay ]; };
 
   rust = pkgs.rust-bin.stable.latest.default.override {
--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,15 @@
+use anyhow::Result;
 use bluer::{
     agent::{self, Agent, ReqResult},
-    Address,
+    Address, Device,
 };
-use futures::StreamExt;
 use std::{
     future::Future,
     io::{Read, Write},
     pin::Pin,
     process::{Command, Stdio},
 };
-
-fn get_pin(req: agent::RequestPinCode) -> Pin<Box<dyn Future<Output = ReqResult<String>> + Send>> {
-    println!("PIN code requested");
-    Box::pin(async move {
-        println!(
-            "Device requesting PIN code for {} on {}",
-            req.device, req.adapter
-        );
-
-        let mut child = Command::new("pinentry-qt")
-            .stdin(Stdio::piped())
-            .stdout(Stdio::piped())
-            .spawn()
-            .unwrap();
-
-        let mut stdin = child.stdin.take().unwrap();
-        let mut stdout = child.stdout.take().unwrap();
-
-        // Write commands to pinentry
-        stdin.write_all(b"SETTITLE Bluetooth PIN\n").unwrap();
-        stdin
-            .write_all(format!("SETDESC Enter PIN for device {}\n", req.device).as_bytes())
-            .unwrap();
-        stdin.write_all(b"GETPIN\n").unwrap();
-
-        // Read response line by line
-        let mut pin = String::new();
-        let mut buf = [0u8; 1024];
-
-        loop {
-            let n = stdout.read(&mut buf).unwrap();
-            if n == 0 {
-                break;
-            }
-
-            let response = String::from_utf8_lossy(&buf[..n]);
-            if response.starts_with("D ") {
-                pin = response[2..8].trim().to_string();
-                break;
-            }
-        }
-
-        child.kill().unwrap();
-
-        if pin.is_empty() {
-            Err(bluer::agent::ReqError::Rejected)
-        } else {
-            Ok(pin)
-        }
-    })
-}
+use uuid::Uuid;
 
 fn get_passkey(req: agent::RequestPasskey) -> Pin<Box<dyn Future<Output = ReqResult<u32>> + Send>> {
     println!("Passkey requested");
@@ -114,76 +64,96 @@ fn get_passkey(req: agent::RequestPasskey) -> Pin<Box<dyn Future<Output = ReqRes
     })
 }
 
-#[tokio::main(flavor = "multi_thread")]
-async fn main() -> bluer::Result<()> {
-    let mac_address = "ED:12:89:6C:08:37";
+#[allow(dead_code)]
+#[rustfmt::skip]
+async fn map_device_endpoints(dev: &Device) -> Result<()> {
+    // SAF Tehnika Service Characteristics (Aranet2 has different readings characteristic uuids)
+    const CHARACTERISTIC_SENSOR_STATE: Uuid = Uuid::from_u128(0xf0cd140195da4f4b9ac8aa55d312af0c);
+    const CHARACTERISTIC_CMD: Uuid = Uuid::from_u128(0xf0cd140295da4f4b9ac8aa55d312af0c);
+    const CHARACTERISTIC_CALIBRATION_DATA: Uuid = Uuid::from_u128(0xf0cd150295da4f4b9ac8aa55d312af0c);
+    const CHARACTERISTIC_CURRENT_READINGS: Uuid = Uuid::from_u128(0xf0cd150395da4f4b9ac8aa55d312af0c);
+    const CHARACTERISTIC_CURRENT_READINGS_AR2: Uuid = Uuid::from_u128(0xf0cd150495da4f4b9ac8aa55d312af0c); // Aranet2 Only
+    const CHARACTERISTIC_TOTAL_READINGS: Uuid = Uuid::from_u128(0xf0cd200195da4f4b9ac8aa55d312af0c);
+    const CHARACTERISTIC_INTERVAL: Uuid = Uuid::from_u128(0xf0cd200295da4f4b9ac8aa55d312af0c);
+    const CHARACTERISTIC_HISTORY_READINGS_V1: Uuid = Uuid::from_u128(0xf0cd200395da4f4b9ac8aa55d312af0c);
+    const CHARACTERISTIC_SECONDS_SINCE_UPDATE: Uuid = Uuid::from_u128(0xf0cd200495da4f4b9ac8aa55d312af0c);
+    const CHARACTERISTIC_HISTORY_READINGS_V2: Uuid = Uuid::from_u128(0xf0cd200595da4f4b9ac8aa55d312af0c);
+    const CHARACTERISTIC_CURRENT_READINGS_DET: Uuid = Uuid::from_u128(0xf0cd300195da4f4b9ac8aa55d312af0c);
+    const CHARACTERISTIC_CURRENT_READINGS_A: Uuid = Uuid::from_u128(0xf0cd300295da4f4b9ac8aa55d312af0c);
+    const CHARACTERISTIC_CURRENT_READINGS_A_AR2: Uuid = Uuid::from_u128(0xf0cd300395da4f4b9ac8aa55d312af0c); // Aranet2 Only
+
+    for service in dev.services().await? {
+        println!(
+            "    Service: {} HexId:{:x}",
+            service.id(),
+            service.uuid().await?
+        );
+        dbg!(&service.all_properties().await);
+        for characteristic in service.characteristics().await? {
+            print!(
+                "        Char: Id: {} Hex: {:x} -> ",
+                characteristic.id(),
+                characteristic.uuid().await?
+            );
+            println!("{:?}", characteristic.cached_value().await);
+            dbg!(&characteristic.all_properties().await);
+            // println!("{:?}", characteristic.read().await);
+        }
+    }
+
+    Ok(())
+}
+
+fn str_mac_to_array(mac_address: &str) -> Result<[u8; 6]> {
     let mut mac_array = [0u8; 6];
 
-    // Convert MAC address string to [u8; 6]
     for (i, part) in mac_address.split(':').enumerate() {
-        mac_array[i] = u8::from_str_radix(part, 16).expect("Invalid hex value");
+        mac_array[i] = u8::from_str_radix(part, 16)?;
     }
+
+    Ok(mac_array)
+}
+
+#[tokio::main(flavor = "multi_thread")]
+async fn main() -> Result<()> {
+    let mac_array = str_mac_to_array("ED:12:89:6C:08:37")?;
 
     // Create a new Bluetooth session
     let session = bluer::Session::new().await?;
 
     let agent = Agent {
-        request_pin_code: Some(Box::new(get_pin)),
         request_passkey: Some(Box::new(get_passkey)),
-        display_pin_code: Some(Box::new(|req| {
-            println!("Pincode display: {}", req.pincode);
-            Box::pin(async move { Ok(()) })
-        })),
-        display_passkey: Some(Box::new(|req| {
-            println!("Passkey display: {}", req.passkey);
-            Box::pin(async move { Ok(()) })
-        })),
         ..Default::default()
     };
 
-    let agent = session.register_agent(agent).await?;
-    let _guard = agent;
+    let _agent = session.register_agent(agent).await?;
 
-    let adapter_names = session.adapter_names().await?;
+    // Get the adapter and power it on
+    let adapter = session.adapter("hci0")?;
+    adapter.set_powered(true).await?;
 
-    for adapter_name in adapter_names {
-        println!("Bluetooth adapter {}:", &adapter_name);
+    let dev = adapter.device(Address::new(mac_array))?;
+    println!("Found device: {:?}", dev);
 
-        // Get the adapter and power it on
-        let adapter = session.adapter(&adapter_name)?;
-        adapter.set_powered(true).await?;
+    if !dev.is_paired().await? {
+        println!("Device is not paired. Attempting to pair...");
 
-        // Start device discovery
-        let _stream = adapter.discover_devices().await?;
-
-        // Spawn a task to handle device discovery events
-        tokio::spawn(async move {
-            let mut stream = _stream;
-            while let Some(item) = stream.next().await {
-                println!("Discovered device: {:?}", item);
-            }
-        });
-
-        // Wait for a moment to allow device discovery
-        tokio::time::sleep(std::time::Duration::from_secs(2)).await;
-
-        // Get the device by its MAC address
-        let dev = adapter.device(Address::new(mac_array))?;
-        println!("Found device: {:?}", dev);
-
-        // Handle pairing
-        if !dev.is_paired().await? {
-            println!("Device is not paired. Attempting to pair...");
-
-            // Start pairing
-            match dev.pair().await {
-                Ok(_) => println!("Pairing successful!"),
-                Err(err) => eprintln!("Pairing failed: {:?}", err),
-            }
-        } else {
-            println!("Device is already paired.");
+        match dev.pair().await {
+            Ok(_) => println!("Pairing successful!"),
+            Err(err) => eprintln!("Pairing failed: {:?}", err),
         }
+    } else {
+        println!("Device is already paired.");
     }
+
+    dev.connect().await?;
+
+    while dev.is_services_resolved().await == Ok(false) {
+        println!("Waiting for service resolve...");
+        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+    }
+
+    map_device_endpoints(&dev).await?;
 
     Ok(())
 }
