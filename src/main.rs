f519f55c13e9566d724faecd2c37e2288aefab5d
Chell L. <pumpkinbiscuit@protonmail.com>
2025-01-31 08:43:52 +0000

    * added Temp type for formatting the raw u16 * added config toml file for adapter and mac address * added endpoint resolution * added parsing for current_readings endpoint * added default display for the readings * added --oneline for status bars

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
+use arrayref::{array_ref, array_refs};
 use bluer::{
     agent::{self, Agent, ReqResult},
+    gatt::remote::Characteristic,
     Address, Device,
 };
+use serde::Deserialize;
 use std::{
+    env,
+    fmt::{Display, Error as FmtError, Formatter},
+    fs,
     future::Future,
     io::{Read, Write},
     pin::Pin,
     process::{Command, Stdio},
+    result::Result as StdResult,
 };
 use uuid::Uuid;
 
@@ -64,44 +71,163 @@ fn get_passkey(req: agent::RequestPasskey) -> Pin<Box<dyn Future<Output = ReqRes
     })
 }
 
+#[derive(Debug)]
+/// Internally represented as 20 * $temp_in_c
+struct Temp(u16);
+
+#[allow(dead_code)]
+impl Temp {
+    /// gives 10 * Int C
+    fn c(&self) -> u16 {
+        self.0 / 2
+    }
+    /// gives 10 * Int F
+    fn f(&self) -> u16 {
+        (((self.0 * 9) / 5) / 2) + 320
+    }
+    fn c_float(&self) -> f32 {
+        self.0 as f32 / 20.0
+    }
+    fn f_float(&self) -> f32 {
+        (self.0 as f32 / 20.0) * (9.0 / 5.0) + 32.0
+    }
+}
+
+impl Display for Temp {
+    fn fmt(&self, f: &mut Formatter) -> StdResult<(), FmtError> {
+        write!(f, "{}", self.0)?;
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+struct CurrentReading {
+    c02: u16,
+    temp: Temp,
+    preasure: u16,
+    humidity: u8,
+    bat: u8,
+    status: u8,
+}
+
+impl Display for CurrentReading {
+    fn fmt(&self, f: &mut Formatter) -> StdResult<(), FmtError> {
+        writeln!(f, "CO2:         {}", self.c02)?;
+        writeln!(
+            f,
+            "Temperature: {:.2}°C / {:.2}°F",
+            self.temp.c_float(),
+            self.temp.f_float(),
+        )?;
+        writeln!(f, "Humidity:    {}", self.humidity)?;
+        writeln!(f, "Presure:     {}", self.preasure)?;
+        writeln!(f, "Battery:     {}", self.bat)?;
+        write!(f, "Status:      {}", self.status)?;
+        Ok(())
+    }
+}
+
+#[derive(Debug, Default)]
+struct EndPoints {
+    battery_level: Option<Characteristic>,
+    sensor_state: Option<Characteristic>,
+    cmd: Option<Characteristic>,
+    calibration_data: Option<Characteristic>,
+    current_readings: Option<Characteristic>,
+    current_readings_ar2: Option<Characteristic>,
+    total_readings: Option<Characteristic>,
+    interval: Option<Characteristic>,
+    history_readings_v1: Option<Characteristic>,
+    seconds_since_update: Option<Characteristic>,
+    history_readings_v2: Option<Characteristic>,
+    current_readings_det: Option<Characteristic>,
+    current_readings_a: Option<Characteristic>,
+    current_readings_a_ar2: Option<Characteristic>,
+}
+
+impl EndPoints {
+    async fn read(&self) -> Result<CurrentReading> {
+        if let Some(c) = &self.current_readings {
+            let bytes = c.read().await?;
+            let src = array_ref![bytes, 0, 9];
+            let (c02, temp, preasure, humidity, bat, status) = array_refs![src, 2, 2, 2, 1, 1, 1];
+            return Ok(CurrentReading {
+                c02: u16::from_le_bytes(*c02),
+                temp: Temp(u16::from_le_bytes(*temp)),
+                preasure: u16::from_le_bytes(*preasure),
+                humidity: humidity[0],
+                bat: bat[0],
+                status: status[0],
+            });
+        }
+        Err(anyhow!("Failed"))
+    }
+}
+
 #[allow(dead_code)]
 #[rustfmt::skip]
-async fn map_device_endpoints(dev: &Device) -> Result<()> {
-    // SAF Tehnika Service Characteristics (Aranet2 has different readings characteristic uuids)
-    const CHARACTERISTIC_SENSOR_STATE: Uuid = Uuid::from_u128(0xf0cd140195da4f4b9ac8aa55d312af0c);
-    const CHARACTERISTIC_CMD: Uuid = Uuid::from_u128(0xf0cd140295da4f4b9ac8aa55d312af0c);
-    const CHARACTERISTIC_CALIBRATION_DATA: Uuid = Uuid::from_u128(0xf0cd150295da4f4b9ac8aa55d312af0c);
-    const CHARACTERISTIC_CURRENT_READINGS: Uuid = Uuid::from_u128(0xf0cd150395da4f4b9ac8aa55d312af0c);
-    const CHARACTERISTIC_CURRENT_READINGS_AR2: Uuid = Uuid::from_u128(0xf0cd150495da4f4b9ac8aa55d312af0c); // Aranet2 Only
-    const CHARACTERISTIC_TOTAL_READINGS: Uuid = Uuid::from_u128(0xf0cd200195da4f4b9ac8aa55d312af0c);
-    const CHARACTERISTIC_INTERVAL: Uuid = Uuid::from_u128(0xf0cd200295da4f4b9ac8aa55d312af0c);
-    const CHARACTERISTIC_HISTORY_READINGS_V1: Uuid = Uuid::from_u128(0xf0cd200395da4f4b9ac8aa55d312af0c);
-    const CHARACTERISTIC_SECONDS_SINCE_UPDATE: Uuid = Uuid::from_u128(0xf0cd200495da4f4b9ac8aa55d312af0c);
-    const CHARACTERISTIC_HISTORY_READINGS_V2: Uuid = Uuid::from_u128(0xf0cd200595da4f4b9ac8aa55d312af0c);
-    const CHARACTERISTIC_CURRENT_READINGS_DET: Uuid = Uuid::from_u128(0xf0cd300195da4f4b9ac8aa55d312af0c);
-    const CHARACTERISTIC_CURRENT_READINGS_A: Uuid = Uuid::from_u128(0xf0cd300295da4f4b9ac8aa55d312af0c);
-    const CHARACTERISTIC_CURRENT_READINGS_A_AR2: Uuid = Uuid::from_u128(0xf0cd300395da4f4b9ac8aa55d312af0c); // Aranet2 Only
+async fn map_device_endpoints(dev: &Device) -> Result<EndPoints> {
+    const SERVICE_GAP: Uuid = Uuid::from_u128(0x0000180000001000800000805f9b34fb);
+    const CHAR_DEVICE_NAME: Uuid = Uuid::from_u128(0x00002a000001000800000805f9b34fb);
+    const CHAR_APPEARANCE: Uuid = Uuid::from_u128(0x00002a010001000800000805f9b34fb);
+
+    const SERVICE_DIS: Uuid = Uuid::from_u128(0x0000180a00001000800000805f9b34fb);
+    const CHAR_SYSTEM_ID: Uuid = Uuid::from_u128(0x00002a230001000800000805f9b34fb);
+    const CHAR_MODEL_NUMBER: Uuid = Uuid::from_u128(0x00002a240001000800000805f9b34fb);
+    const CHAR_SERIAL_NO: Uuid = Uuid::from_u128(0x00002a250001000800000805f9b34fb);
+    const CHAR_SW_REV: Uuid = Uuid::from_u128(0x00002a260001000800000805f9b34fb);
+    const CHAR_HW_REV: Uuid = Uuid::from_u128(0x00002a270001000800000805f9b34fb);
+    const CHAR_SW_REV_FACTORY: Uuid = Uuid::from_u128(0x00002a280001000800000805f9b34fb);
+    const CHAR_MANUFACTURER_NAME: Uuid = Uuid::from_u128(0x00002a290001000800000805f9b34fb);
+
+    const SERVICE_BATTTERY: Uuid = Uuid::from_u128(0x0000180f00001000800000805f9b34fb); // v1.2.0 and later
+    const CHAR_BATTERY_LEVEL: Uuid = Uuid::from_u128(0x00002a190001000800000805f9b34fb);
+
+    const SERVICE_SAF_TEHNIKA: Uuid = Uuid::from_u128(0x0000fce000001000800000805f9b34fb); // v1.2.0 and later
+    const CHAR_SENSOR_STATE: Uuid = Uuid::from_u128(0xf0cd140195da4f4b9ac8aa55d312af0c);
+    const CHAR_CMD: Uuid = Uuid::from_u128(0xf0cd140295da4f4b9ac8aa55d312af0c);
+    const CHAR_CALIBRATION_DATA: Uuid = Uuid::from_u128(0xf0cd150295da4f4b9ac8aa55d312af0c);
+    const CHAR_CURRENT_READINGS: Uuid = Uuid::from_u128(0xf0cd150395da4f4b9ac8aa55d312af0c);
+    const CHAR_CURRENT_READINGS_AR2: Uuid = Uuid::from_u128(0xf0cd150495da4f4b9ac8aa55d312af0c); // Aranet2 Only
+    const CHAR_TOTAL_READINGS: Uuid = Uuid::from_u128(0xf0cd200195da4f4b9ac8aa55d312af0c);
+    const CHAR_INTERVAL: Uuid = Uuid::from_u128(0xf0cd200295da4f4b9ac8aa55d312af0c);
+    const CHAR_HISTORY_READINGS_V1: Uuid = Uuid::from_u128(0xf0cd200395da4f4b9ac8aa55d312af0c);
+    const CHAR_SECONDS_SINCE_UPDATE: Uuid = Uuid::from_u128(0xf0cd200495da4f4b9ac8aa55d312af0c);
+    const CHAR_HISTORY_READINGS_V2: Uuid = Uuid::from_u128(0xf0cd200595da4f4b9ac8aa55d312af0c);
+    const CHAR_CURRENT_READINGS_DET: Uuid = Uuid::from_u128(0xf0cd300195da4f4b9ac8aa55d312af0c);
+    const CHAR_CURRENT_READINGS_A: Uuid = Uuid::from_u128(0xf0cd300295da4f4b9ac8aa55d312af0c);
+    const CHAR_CURRENT_READINGS_A_AR2: Uuid = Uuid::from_u128(0xf0cd300395da4f4b9ac8aa55d312af0c); // Aranet2 Only
+
+    const SERVICE_NORDIC_SEMICONDUCTOR: Uuid = Uuid::from_u128(0x0000fe5900001000800000805f9b34fb);
+    const CHAR_SECURE_DFU: Uuid = Uuid::from_u128(0x8ec90003f3154f609fb8838830daea50);
+
+    let mut endpoint = EndPoints::default();
 
     for service in dev.services().await? {
-        println!(
-            "    Service: {} HexId:{:x}",
-            service.id(),
-            service.uuid().await?
-        );
-        dbg!(&service.all_properties().await);
+        let service_uuid = service.uuid().await?;
         for characteristic in service.characteristics().await? {
-            print!(
-                "        Char: Id: {} Hex: {:x} -> ",
-                characteristic.id(),
-                characteristic.uuid().await?
-            );
-            println!("{:?}", characteristic.cached_value().await);
-            dbg!(&characteristic.all_properties().await);
-            // println!("{:?}", characteristic.read().await);
+            let characteristic_uuid = characteristic.uuid().await?;
+            match (service_uuid, characteristic_uuid) {
+                (SERVICE_BATTTERY, CHAR_BATTERY_LEVEL) => endpoint.battery_level = Some(characteristic),
+                (SERVICE_SAF_TEHNIKA, CHAR_SENSOR_STATE) => endpoint.sensor_state = Some(characteristic),
+                (SERVICE_SAF_TEHNIKA, CHAR_CMD) => endpoint.cmd = Some(characteristic),
+                (SERVICE_SAF_TEHNIKA, CHAR_CALIBRATION_DATA) => endpoint.calibration_data = Some(characteristic),
+                (SERVICE_SAF_TEHNIKA, CHAR_CURRENT_READINGS) => endpoint.current_readings = Some(characteristic),
+                (SERVICE_SAF_TEHNIKA, CHAR_CURRENT_READINGS_AR2) => endpoint.current_readings_ar2 = Some(characteristic),
+                (SERVICE_SAF_TEHNIKA, CHAR_TOTAL_READINGS) => endpoint.total_readings = Some(characteristic),
+                (SERVICE_SAF_TEHNIKA, CHAR_INTERVAL) => endpoint.interval = Some(characteristic),
+                (SERVICE_SAF_TEHNIKA, CHAR_HISTORY_READINGS_V1) => endpoint.history_readings_v1 = Some(characteristic),
+                (SERVICE_SAF_TEHNIKA, CHAR_SECONDS_SINCE_UPDATE) => endpoint.seconds_since_update = Some(characteristic),
+                (SERVICE_SAF_TEHNIKA, CHAR_HISTORY_READINGS_V2) => endpoint.history_readings_v2 = Some(characteristic),
+                (SERVICE_SAF_TEHNIKA, CHAR_CURRENT_READINGS_DET) => endpoint.current_readings_det = Some(characteristic),
+                (SERVICE_SAF_TEHNIKA, CHAR_CURRENT_READINGS_A) => endpoint.current_readings_a = Some(characteristic),
+                (SERVICE_SAF_TEHNIKA, CHAR_CURRENT_READINGS_A_AR2) => endpoint.current_readings_a_ar2 = Some(characteristic),
+                (_, _) => {},
+            }
         }
     }
 
-    Ok(())
+    Ok(endpoint)
 }
 
 fn str_mac_to_array(mac_address: &str) -> Result<[u8; 6]> {
@@ -114,26 +240,39 @@ fn str_mac_to_array(mac_address: &str) -> Result<[u8; 6]> {
     Ok(mac_array)
 }
 
+#[derive(Deserialize)]
+pub struct Cfg {
+    /// EX: hci0
+    pub adapter: String,
+    /// FORMAT: ED:12:89:6C:08:37
+    pub mac: String,
+}
+
+pub fn try_get_cfg() -> Result<Cfg> {
+    let home = env::var("HOME")?;
+    let content = fs::read_to_string(format!("{home}/.config/aranet/config.toml"))?;
+    let config = toml::from_str::<Cfg>(&content)?;
+    Ok(config)
+}
+
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<()> {
-    let mac_array = str_mac_to_array("ED:12:89:6C:08:37")?;
+    let cfg = try_get_cfg()?;
+    let mac_array = str_mac_to_array(&cfg.mac)?;
 
-    // Create a new Bluetooth session
     let session = bluer::Session::new().await?;
 
-    let agent = Agent {
-        request_passkey: Some(Box::new(get_passkey)),
-        ..Default::default()
-    };
+    let _agent = session
+        .register_agent(Agent {
+            request_passkey: Some(Box::new(get_passkey)),
+            ..Default::default()
+        })
+        .await?;
 
-    let _agent = session.register_agent(agent).await?;
-
-    // Get the adapter and power it on
-    let adapter = session.adapter("hci0")?;
+    let adapter = session.adapter(&cfg.adapter)?;
     adapter.set_powered(true).await?;
 
     let dev = adapter.device(Address::new(mac_array))?;
-    println!("Found device: {:?}", dev);
 
     if !dev.is_paired().await? {
         println!("Device is not paired. Attempting to pair...");
@@ -142,18 +281,35 @@ async fn main() -> Result<()> {
             Ok(_) => println!("Pairing successful!"),
             Err(err) => eprintln!("Pairing failed: {:?}", err),
         }
-    } else {
-        println!("Device is already paired.");
     }
 
     dev.connect().await?;
 
-    while dev.is_services_resolved().await == Ok(false) {
-        println!("Waiting for service resolve...");
-        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
-    }
+    // while dev.is_services_resolved().await == Ok(false) {
+    // println!("Waiting for service resolve...");
+    // tokio::time::sleep(std::time::Duration::from_millis(20)).await;
+    // }
 
-    map_device_endpoints(&dev).await?;
+    let endpoint = map_device_endpoints(&dev).await?;
+    let readings = endpoint.read().await?;
+
+    if let Some(arg) = env::args().skip(1).next() {
+        match arg.as_str() {
+            "--oneline" => {
+                println!(
+                    "{}ppm {:.2}°F {}% {}hPa",
+                    readings.c02,
+                    readings.temp.f_float(),
+                    readings.humidity,
+                    readings.preasure / 10
+                );
+            }
+            "--service" => todo!(),
+            _ => {}
+        }
+    } else {
+        println!("{}", readings);
+    }
 
     Ok(())
 }
